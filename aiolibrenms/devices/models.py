3376069ebed2378c2b7a440866c115f1f7811d69
mib1185 <mail@mib85.de>
2026-07-13 21:25:15 +0000

    fix LibrenmsDeviceInfo model

--- aiolibrenms/devices/models.py
+++ aiolibrenms/devices/models.py
@@ -14,72 +14,78 @@ class LibrenmsDeviceInfo(DataClassJSONMixin):
     """Representation of the librenms device information."""
 
     # non-default fields
-    device_id: int
-    inserted: datetime
-    hostname: str
-    sys_name: str = field(metadata=field_options(alias="sysName"))
-    display: str
-    ip: str
-    community: str
-    snmpver: str
-    port: int
-    transport: str
-    snmp_disable: bool
-    sys_object_id: str = field(metadata=field_options(alias="sysObjectID"))
-    sys_descr: str = field(metadata=field_options(alias="sysDescr"))
-    sys_contact: str = field(metadata=field_options(alias="sysContact"))
-    version: str
-    hardware: str
-    features: str
-    os: str
-    status: bool
-    status_reason: str
-    ignore: bool
-    disabled: bool
     agent_uptime: int
-    last_polled: datetime
-    last_polled_timetaken: float
-    last_discovered_timetaken: float
-    type: str
+    device_id: int
+    disable_notify: bool
+    disabled: bool
+    display: str
+    features: str
+    hardware: str
+    hostname: str
     icon: str
-    poller_group: int
+    ignore_status: bool
+    ignore: bool
+    inserted: datetime
+    ip: str
+    last_discovered_timetaken: float
+    last_polled_timetaken: float
+    last_polled: datetime
+    max_depth: int
+    mtu_status: bool
+    os: str
     override_sys_location: bool = field(
         metadata=field_options(alias="override_sysLocation")
     )
+    poller_group: int
     port_association_mode: int
-    max_depth: int
-    disable_notify: bool
-    ignore_status: bool
-    mtu_status: bool
+    port: int
+    snmp_disable: bool
+    snmpver: str
+    status_reason: str
+    status: bool
+    sys_name: str = field(metadata=field_options(alias="sysName"))
+    transport: str
+    type: str
+    version: str
 
     # default fields
-    display_template: str | None = field(default=None)
-    overwrite_ip: str | None = field(default=None)
+    authalgo: str | None = field(default=None)
     authlevel: str | None = field(default=None)
     authname: str | None = field(default=None)
     authpass: str | None = field(default=None)
-    authalgo: str | None = field(default=None)
-    cryptopass: str | None = field(default=None)
-    cryptoalgo: str | None = field(default=None)
-    location_id: int | None = field(default=None)
-    uptime: int | None = field(default=None)
-    timeout: str | None = field(default=None)
-    retries: str | None = field(default=None)
     bgp_local_as: str | None = field(
         default=None, metadata=field_options(alias="bgpLocalAs")
     )
+    community: str | None = field(default=None)
+    cryptoalgo: str | None = field(default=None)
+    cryptopass: str | None = field(default=None)
+    dependency_parent_hostname: str | None = field(default=None)
+    dependency_parent_id: str | None = field(default=None)
+    display_template: str | None = field(default=None)
+    last_discovered: datetime | None = field(default=None)
+    last_ping_timetaken: float | None = field(default=None)
+    last_ping: datetime | None = field(default=None)
+    last_poll_attempted: datetime | None = field(default=None)
+    lat: float | None = field(default=None)
+    lng: float | None = field(default=None)
+    location_id: int | None = field(default=None)
+    location: str | None = field(default=None)
+    notes: str | None = field(default=None)
+    overwrite_ip: str | None = field(default=None)
+    purpose: str | None = field(default=None)
+    retries: str | None = field(default=None)
+    serial: str | None = field(default=None)
     snmp_engine_id: str | None = field(
         default=None, metadata=field_options(alias="snmpEngineID")
     )
-    last_poll_attempted: datetime | None = field(default=None)
-    last_discovered: datetime | None = field(default=None)
-    last_ping: datetime | None = field(default=None)
-    last_ping_timetaken: float | None = field(default=None)
-    purpose: str | None = field(default=None)
-    serial: str | None = field(default=None)
-    notes: str | None = field(default=None)
-    dependency_parent_id: str | None = field(default=None)
-    dependency_parent_hostname: str | None = field(default=None)
-    location: str | None = field(default=None)
-    lat: float | None = field(default=None)
-    lng: float | None = field(default=None)
+    sys_contact: str | None = field(
+        default=None, metadata=field_options(alias="sysContact")
+    )
+    sys_descr: str | None = field(
+        default=None, metadata=field_options(alias="sysDescr")
+    )
+    sys_object_id: str | None = field(
+        default=None, metadata=field_options(alias="sysObjectID")
+    )
+    timeout: str | None = field(default=None)
+    uptime: int | None = field(default=None)
