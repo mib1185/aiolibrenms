7313400c366d28a1f128983579c08ef26258eb74
mib1185 <mail@mib85.de>
2026-07-13 21:20:38 +0000

    fix typo

--- tests/test_system.py
+++ tests/test_system.py
@@ -10,7 +10,7 @@ from aiolibrenms.system.models import LibrenmsSystemInfo
 async def test_get_system_info(mock_librenms_with_data, snapshot: SnapshotAssertion):
     """Test async_get_system_info."""
     api = await mock_librenms_with_data()
-    systm_info = await api.system.async_get_system_info()
+    system_info = await api.system.async_get_system_info()
 
-    assert isinstance(systm_info, LibrenmsSystemInfo)
-    assert systm_info == snapshot
+    assert isinstance(system_info, LibrenmsSystemInfo)
+    assert system_info == snapshot
