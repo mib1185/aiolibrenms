9b7984562f8d57817ff822b17c586b0594b77520
mib1185 <mail@mib85.de>
2026-07-13 21:26:54 +0000

    fix LibrenmsSystemInfo model

--- aiolibrenms/system/models.py
+++ aiolibrenms/system/models.py
@@ -12,12 +12,13 @@ class LibrenmsSystemInfo(DataClassJSONMixin):
     """Representation of the librenms system information."""
 
     # non-default parameters
-    local_ver: str
-    local_sha: str
-    local_date: str
-    local_branch: str
-    db_schema: str
-    php_ver: str
     database_ver: str
-    rrdtool_ver: str
+    db_schema: str
+    local_branch: str
+    local_date: str
+    local_sha: str
+    local_ver: str
     netsnmp_ver: str
+    php_ver: str
+    python_ver: str
+    rrdtool_ver: str
